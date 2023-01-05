76dad5a92186fda54dd28d24d6b7c98a7a3268c6
val-marinho <ea.valesca.hutcherson@gmail.com>
2023-01-04 22:00:36 -0300

    alterando porta

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const port = process.env.port || 3001;
 
 server.use(middlewares)
 server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+
+server.listen(port)
